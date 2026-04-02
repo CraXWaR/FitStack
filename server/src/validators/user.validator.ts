f555225fae5e062f565f530efd09c89dc99b4c6a
CraXWaR <craxwar@gmail.com>
2026-04-02 09:51:57 +0300

    update: removed the basic 3 chars password validation and added regex one, removed the default browser eye for input passwords

--- server/src/validators/user.validator.ts
+++ server/src/validators/user.validator.ts
@@ -1,6 +1,5 @@
 import {z} from "zod";
 
-//TODO USE THE REGEX WHEN FINISHING APP
 const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
 
 export const RegisterValidateSchema = z.object({
@@ -14,13 +13,12 @@ export const RegisterValidateSchema = z.object({
 
     password: z
         .string()
-        .min(3, "Password must be at least 3 characters long"),
-        // .regex(
-        //     passwordRegex,
-        //     "Password must be at least 8 characters, include 1 uppercase, 1 lowercase, 1 number, and 1 special character"
-        // ),
+        .regex(
+            passwordRegex,
+            "Password must be at least 8 characters, include 1 uppercase, 1 lowercase, 1 number, and 1 special character"
+        ),
 
     confirmPassword: z
         .string()
-        .min(1, "Please confirm your password"),
+        .min(1, "Password doesn't match"),
 });
