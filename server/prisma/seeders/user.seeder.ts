607ad4ccefdb8d2eda86a05667d5e4cd351cc47f
CraXWaR <craxwar@gmail.com>
2026-05-25 10:10:08 +0300

    update: update user seed to update on more seeding

--- server/prisma/seeders/user.seeder.ts
+++ server/prisma/seeders/user.seeder.ts
@@ -4,16 +4,18 @@ import bcrypt from "bcrypt";
 export async function seedUser(prisma: PrismaClient) {
     console.log("Seeding user...");
 
-    const password = await bcrypt.hash('123',12);
+    const password = await bcrypt.hash('123', 12);
 
-    await prisma.user.create({
-        data: {
+    await prisma.user.upsert({
+        where: {email: "test@gmail.com"},
+        update: {},
+        create: {
             email: "test@gmail.com",
             password,
             firstName: "Test",
             lastName: "User"
-        }
+        },
     });
-
+    
     console.log("User created");
 }
